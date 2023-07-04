6465cadafb044c90c693764807889f620ee6ba8d
AlexRyshkov <alexryshkov1000.7@gmail.com>
2023-07-04 21:03:13 +0300

    organize assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from './components/layout/index.jsx'
 import Main from './pages/main/index.jsx'
-
+import './styles.css'
 function App() {
     return (
         <Layout>
