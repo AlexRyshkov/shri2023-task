a01511886875e52001759562161318b1d132ebcb
AlexRyshkov <alexryshkov1000.7@gmail.com>
2023-07-04 21:51:40 +0300

    clean up unused styles and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from './components/layout/index.jsx'
 import Main from './pages/main/index.jsx'
-import './styles.css'
 function App() {
     return (
         <Layout>
